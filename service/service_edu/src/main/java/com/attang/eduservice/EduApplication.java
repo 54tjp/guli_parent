8e796a731a1c601d38760d8ef92816f4fd27de17
tang <1798330829@qq.com>
2022-03-09 19:34:49 +0800

    master commit3

--- service/service_edu/src/main/java/com/attang/eduservice/EduApplication.java
+++ service/service_edu/src/main/java/com/attang/eduservice/EduApplication.java
@@ -15,6 +15,7 @@ import org.springframework.context.annotation.ComponentScan;
 public class EduApplication {
     public static void main(String[] args) {
         System.out.println("冲突合并");
+        System.out.println("测试合并冲突！");
         SpringApplication.run(EduApplication.class,args);
     }
 }
